f4c877be242cfe75d30863a6a5c3eaf3d6e00b72
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-05-26 02:02:06 +0900

    必須関数、プロパティ実装

--- AvailableGamesList/AvailableGamesListMain.cs
+++ AvailableGamesList/AvailableGamesListMain.cs
@@ -1,8 +1,23 @@
+using System.Collections.Generic;
 using ThLaunchSite.Plugin;
 
 namespace AvailableGamesList
 {
-    public class AvailableGamesListMain
+    public class AvailableGamesListMain : GameFilesPluginBase
     {
+        public override string Name => "有効なゲームの一覧";
+
+        public override string Version => "1.0.0";
+
+        public override string Developer => "Mashiro Tamane";
+
+        public override string Description => "東方管制塔 NX にパスが登録されているゲームとその実行ファイルパスをリスト化します。";
+
+        public override string CommandName => "有効なゲームの一覧";
+
+        public override void Main(List<string> availableGamesList, Dictionary<string, string> availableGameFilesDictionary)
+        {
+            
+        }
     }
 }

0a9d11330e011c0f1c4d157d9cc55d627dc66487
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-05-26 02:16:02 +0900

    Main()実装

--- AvailableGamesList/AvailableGamesListMain.cs
+++ AvailableGamesList/AvailableGamesListMain.cs
@@ -15,9 +15,48 @@ namespace AvailableGamesList
 
         public override string CommandName => "有効なゲームの一覧";
 
+        private readonly static Dictionary<string, string> _gameNameDictionary = new()
+        {
+            { "Th06", "東方紅魔郷" },
+            { "Th07", "東方妖々夢" },
+            { "Th08", "東方永夜抄" },
+            { "Th09", "東方花映塚" },
+            { "Th095", "東方文花帖" },
+            { "Th10", "東方風神録" },
+            { "Th11", "東方地霊殿" },
+            { "Th12", "東方星蓮船" },
+            { "Th125", "ダブルスポイラー" },
+            { "Th128", "妖精大戦争" },
+            { "Th13", "東方神霊廟" },
+            { "Th14", "東方輝針城" },
+            { "Th143", "弾幕アマノジャク" },
+            { "Th15", "東方紺珠伝" },
+            { "Th16", "東方天空璋" },
+            { "Th165", "秘封ナイトメアダイアリー" },
+            { "Th17", "東方鬼形獣" },
+            { "Th18", "東方虹龍洞" },
+            { "Th185", "バレットフィリア達の闇市場" },
+            { "Th19", "東方獣王園" }
+        };
+
         public override void Main(List<string> availableGamesList, Dictionary<string, string> availableGameFilesDictionary)
         {
-            
+            Dictionary<string, string> availableGamesNameFileDictionary = [];
+
+            if (availableGamesList != null && availableGamesList.Count > 0)
+            {
+                foreach (string gameId in availableGamesList)
+                {
+                    availableGamesNameFileDictionary.Add(gameId, availableGameFilesDictionary[gameId]);
+                }
+            }
+
+            AvailableGamesDialog availableGamesDialog = new()
+            {
+                AvailableGamesPathDictionary = availableGamesNameFileDictionary
+            };
+
+            availableGamesDialog.ShowDialog();
         }
     }
 }

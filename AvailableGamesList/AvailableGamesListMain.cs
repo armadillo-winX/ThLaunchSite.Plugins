8f1d4986f8057b859ee5a628f58b4cc6dad7e799
珠音茉白 <80902436+armadillo-winX@users.noreply.github.com>
2024-05-26 02:41:27 +0900

    プロパティへの代入処理再実装

--- AvailableGamesList/AvailableGamesListMain.cs
+++ AvailableGamesList/AvailableGamesListMain.cs
@@ -64,6 +64,7 @@ namespace AvailableGamesList
             }
             else
             {
+                _availableGamesDialog.AvailableGamesNameFileDictionary = availableGamesNameFileDictionary;
                 _availableGamesDialog.WindowState = System.Windows.WindowState.Normal;
                 _availableGamesDialog.Activate();
             }
